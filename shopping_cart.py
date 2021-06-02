d72530c804f984187df2ac2c6293bb2486dbef20
cys256 <84469159+cys256@users.noreply.github.com>
2021-06-01 21:07:37 -0400

    In-class update to repo

--- shopping_cart.py
+++ shopping_cart.py
@@ -40,3 +40,32 @@ def to_usd(my_price):
 # TODO: write some Python code here to produce the desired output
 
 print(products)
+
+
+# 1) capture product ids until we're done
+# (use infinite while loop)
+
+selected_ids = []
+while True: 
+    selected_id = input("Please select / scan a valid product id: ")
+    if selected_id.upper() == "DONE":
+        break
+    else:
+        selected_ids.append(selected_id)
+    print(selected_id)
+
+print("WE HAVE REACHED THE END OF THE LOOP")
+print(selected_ids)
+
+
+# 2) Perform product lookups to determine what the product's name and price are
+#selected_ids = ["1","2","3","2","1"]
+
+for selected_id in selected_ids:
+    print(selected_id)
+    # lookup the corresponding product!
+    # or maybe display the selected product's name and price
+
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    print(matching_product["name"], matching_product["price"])
